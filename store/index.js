3c7e36ad17bd86be93f1bc638fa1d3e5af2c4ad4
developjik <developjik@gmail.com>
2022-05-20 22:42:00 +0900

    feat: mobx example 추가

---
+++ store/index.js
@@ -0,0 +1,8 @@
+import countStore1 from "store/count1";
+import countStore2 from "store/count2";
+import countStore3 from "store/count3";
+import doubleStore from "store/double";
+
+const store = {countStore1, countStore2, countStore3, doubleStore}
+
+export default store
